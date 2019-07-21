7b2a528db4b948b0a4699644a5d926e778c828b8
Jon Moore <jonm@jjmoore.net>
2019-07-21 18:23:11 -0400

    Upgrade to Terraform 0.12.

--- s3pypi.tf
+++ s3pypi.tf
@@ -1,8 +1,12 @@
 variable "region" {
   default = "us-east-1"
 }
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
+
+variable "aws_access_key" {
+}
+
+variable "aws_secret_key" {
+}
 
 terraform {
   backend "s3" {
@@ -11,10 +15,10 @@ terraform {
 }
 
 provider "aws" {
-  region = "${var.region}"
-  version = "~> 2.11"
-  access_key = "${var.aws_access_key}"
-  secret_key = "${var.aws_secret_key}"
+  region     = var.region
+  version    = "~> 2.11"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
 }
 
 # resource "aws_s3_bucket" "artifact_bucket" {
@@ -24,3 +28,4 @@ provider "aws" {
 resource "aws_s3_bucket" "index_bucket" {
   bucket = "s3pypi-index"
 }
+
