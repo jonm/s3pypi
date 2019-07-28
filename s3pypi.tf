0f81a49ff3e62b1101a443c48294e02806323134
Jon Moore <jonm@jjmoore.net>
2019-07-28 15:22:22 -0400

    Upgrade terraform config to 0.12 compatible version.

--- s3pypi.tf
+++ s3pypi.tf
@@ -4,17 +4,11 @@ variable "region" {
 variable "aws_access_key" {}
 variable "aws_secret_key" {}
 
-terraform {
-  backend "s3" {
-    key = "s3pypi.tfstate"
-  }
-}
-
 provider "aws" {
-  region = "${var.region}"
+  region = var.region
   version = "~> 2.11"
-  access_key = "${var.aws_access_key}"
-  secret_key = "${var.aws_secret_key}"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
 }
 
 resource "aws_s3_bucket" "artifact_bucket" {
